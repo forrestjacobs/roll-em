3275dc17830dfc5d2a0001fb9d379711b1285248
Forrest Jacobs <forrestjacobs@gmail.com>
2020-05-20 21:39:22 -0400

    Test refactor

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,64 +1,39 @@
+import { NumberTerm, RollTerm } from "./Formula";
 import { sum } from "./sum";
 
+const term2: NumberTerm = {
+  sign: 1,
+  type: "number",
+  value: 2,
+};
+
+const term2d4rolled1and4: RollTerm = {
+  sign: 1,
+  type: "roll",
+  count: 2,
+  sides: 4,
+  value: [1, 4],
+};
+
 test("It can sum a number", () => {
-  expect(
-    sum([
-      {
-        sign: 1,
-        type: "number",
-        value: 2,
-      },
-    ])
-  ).toBe(2);
+  expect(sum([term2])).toBe(2);
 });
 
 test("It can sum a roll", () => {
-  expect(
-    sum([
-      {
-        sign: 1,
-        type: "roll",
-        count: 2,
-        sides: 4,
-        value: [1, 4]
-      },
-    ])
-  ).toBe(5);
+  expect(sum([term2d4rolled1and4])).toBe(5);
 });
 
 test("It can sum multiple terms", () => {
-  expect(
-    sum([
-      {
-        sign: 1,
-        type: "roll",
-        count: 2,
-        sides: 4,
-        value: [1, 4]
-      },
-      {
-        sign: 1,
-        type: "number",
-        value: 2,
-      },
-    ])
-  ).toBe(7);
+  expect(sum([term2d4rolled1and4, term2])).toBe(7);
 });
 
 test("It can subtract terms", () => {
   expect(
     sum([
+      term2d4rolled1and4,
       {
-        sign: 1,
-        type: "roll",
-        count: 2,
-        sides: 4,
-        value: [1, 4]
-      },
-      {
+        ...term2,
         sign: -1,
-        type: "number",
-        value: 2,
       },
     ])
   ).toBe(3);
