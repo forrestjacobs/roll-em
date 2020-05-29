2916beced4ceac3bd56c50090ddce651a3bbd222
Forrest Jacobs <forrestjacobs@gmail.com>
2020-05-29 14:17:36 -0400

    Match entire string in Result test

--- src/ui/Result.test.ts
+++ src/ui/Result.test.ts
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/svelte";
+import escapeStringRegexp from "escape-string-regexp";
 import { NumberTerm, RollTerm } from "../formula";
 import Result from "./Result.svelte";
 
@@ -21,35 +22,39 @@ const term2d4rolled1and4: RollTerm = {
   value: [1, 4],
 };
 
+function matchExactly(string: string) {
+  return new RegExp(`^${escapeStringRegexp(string)}$`);
+}
+
 test("It defaults to blank", () => {
   const { container } = render(Result);
-  expect(container).toHaveTextContent("(empty) = 999");
+  expect(container).toHaveTextContent(matchExactly("(empty) = 999"));
 });
 
 test("It renders a number", () => {
   const { container } = render(Result, {
     result: [term2],
   });
-  expect(container).toHaveTextContent("2 = 999");
+  expect(container).toHaveTextContent(matchExactly("2 = 999"));
 });
 
 test("It renders a dice formula", () => {
   const { container } = render(Result, {
     result: [term2d4rolled1and4],
   });
-  expect(container).toHaveTextContent("(2d4: 1 + 4) = 999");
+  expect(container).toHaveTextContent(matchExactly("(2d4: 1 + 4) = 999"));
 });
 
 test("It renders multiple terms", () => {
   const { container } = render(Result, {
     result: [term2d4rolled1and4, term2],
   });
-  expect(container).toHaveTextContent("(2d4: 1 + 4) + 2 = 999");
+  expect(container).toHaveTextContent(matchExactly("(2d4: 1 + 4) + 2 = 999"));
 });
 
 test("It renders subtracted terms", () => {
   const { container } = render(Result, {
     result: [term2d4rolled1and4, { ...term2, sign: -1 }],
   });
-  expect(container).toHaveTextContent("(2d4: 1 + 4) - 2 = 999");
+  expect(container).toHaveTextContent(matchExactly("(2d4: 1 + 4) - 2 = 999"));
 });
