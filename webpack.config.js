d5e49e935c0aa71e922aee887f37461554fc6b05
Forrest Jacobs <forrestjacobs@gmail.com>
2020-07-17 16:23:05 -0400

    Fix js & css inlining

--- webpack.config.js
+++ webpack.config.js
@@ -7,62 +7,66 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  entry: "./src/index.ts",
-  optimization: {
-    minimizer: [
-      new TerserPlugin(),
-      new OptimizeCSSAssetsPlugin({})
-    ]
-  },
-  plugins: [
+module.exports = function (eng, argv) {
+  const plugins = [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "Roll 'Em",
       inlineSource: ".(js|css)$",
     }),
-    isProd && new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin),
     new MiniCssExtractPlugin(),
-  ].filter(p => p),
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.pegjs$/,
-        exclude: /node_modules/,
-        loader: "pegjs-loader",
-      },
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: "ts-loader",
-      },
-      {
-        test: /\.svelte$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "svelte-loader",
-          options: {
-            emitCss: true,
+  ];
+  if (argv.mode === "production") {
+    plugins.push(new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin));
+  }
+
+  return {
+    entry: "./src/index.ts",
+    optimization: {
+      minimizer: [
+        new TerserPlugin(),
+        new OptimizeCSSAssetsPlugin({})
+      ]
+    },
+    plugins,
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.pegjs$/,
+          exclude: /node_modules/,
+          loader: "pegjs-loader",
+        },
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: "ts-loader",
+        },
+        {
+          test: /\.svelte$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "svelte-loader",
+            options: {
+              emitCss: true,
+            },
           },
         },
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-    ],
-  },
-  resolve: {
-    alias: {
-      svelte: path.resolve("node_modules", "svelte"),
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+      ],
     },
-    extensions: [".ts", ".svelte", ".mjs", ".js"],
-    mainFields: ["svelte", "browser", "module", "main"],
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-  },
+    resolve: {
+      alias: {
+        svelte: path.resolve("node_modules", "svelte"),
+      },
+      extensions: [".ts", ".svelte", ".mjs", ".js"],
+      mainFields: ["svelte", "browser", "module", "main"],
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+    },
+  };
 };
