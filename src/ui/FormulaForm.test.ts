b2a05ce174722751b8f1b6dc026fb4a11e4989f6
Forrest Jacobs <forrestjacobs@gmail.com>
2020-07-28 21:26:29 -0400

    Test form errors and examples

--- src/ui/FormulaForm.test.ts
+++ src/ui/FormulaForm.test.ts
@@ -1,17 +1,25 @@
 import "@testing-library/jest-dom";
-import { render, fireEvent } from "@testing-library/svelte";
+import { fireEvent, render } from "@testing-library/svelte";
+import type { Editor, TextMarker } from "codemirror";
 import type { Formula } from "../formula";
 import { resultsStore } from "../stores";
 import type { StoredResult } from "../stores/results";
+import { makeEditor, markError } from "./editor";
 import FormulaForm from "./FormulaForm.svelte";
-import { makeEditor } from "./editor";
-import type { Editor } from "codemirror";
 
-const d6result = [{ type: "roll", count: 1, sides: 6, value: [6] }];
+const resultd6 = [{ type: "roll", count: 1, sides: 6, value: [6] }];
+const result2d6 = [{ type: "roll", count: 2, sides: 6, value: [6] }];
+
+type LocationError = Error & { location?: string };
+
+const locationError: LocationError = new Error("Error at location");
+locationError.location = "5,7";
 
 const mockParseResults: { [text: string]: Formula | Error } = {
   d6: [{ type: "roll", count: 1, sides: 6 }],
+  "2d6": [{ type: "roll", count: 2, sides: 6 }],
   d: new Error("Unexpected value"),
+  loc: locationError,
 };
 
 jest.mock("../formula", () => ({
@@ -75,7 +83,7 @@ test("It can roll", async () => {
   editor.setValue("d6");
   await fireEvent.click(result.getByText("Roll"));
 
-  expect(resultsStore.append).toBeCalledWith(d6result);
+  expect(resultsStore.append).toBeCalledWith(resultd6);
   expect(editor.execCommand).toBeCalledWith("selectAll");
 });
 
@@ -87,7 +95,7 @@ test("It can roll by pressing enter", () => {
   editor.setValue("d6");
   editor.getSubmit()();
 
-  expect(resultsStore.append).toBeCalledWith(d6result);
+  expect(resultsStore.append).toBeCalledWith(resultd6);
 });
 
 test("It shows error messages", async () => {
@@ -99,6 +107,38 @@ test("It shows error messages", async () => {
   await fireEvent.click(result.getByText("Roll"));
 
   expect(result.getByText("Unexpected value")).toBeTruthy();
+});
+
+test("It marks errors when there's a location", async () => {
+  mockResults([]);
+  const editor = mockEditor();
+
+  const result = render(FormulaForm);
+  editor.setValue("loc");
+  await fireEvent.click(result.getByText("Roll"));
+
+  expect(result.getByText("Error at location")).toBeTruthy();
+  expect(markError).toBeCalledWith(editor, "5,7");
+});
+
+test("It clears errors on the next roll", async () => {
+  mockResults([]);
+  const editor = mockEditor();
+  const clear = jest.fn();
+  ((markError as unknown) as jest.MockedFunction<
+    typeof markError
+  >).mockReturnValue({
+    clear,
+  } as unknown as TextMarker);
+
+  const result = render(FormulaForm);
+  editor.setValue("loc");
+  await fireEvent.click(result.getByText("Roll"));
+
+  editor.setValue("d6");
+  await fireEvent.click(result.getByText("Roll"));
+  expect(result.queryByText("Error at location")).toBe(null);
+  expect(clear).toBeCalled();
 });
 
 test("It shows examples when there are no results", async () => {
@@ -128,4 +168,16 @@ test("It hides examples when there are errors", async () => {
   await fireEvent.click(result.getByText("Roll"));
 
   expect(result.container).not.toHaveTextContent("Examples:");
+});
+
+test("It rolls an example when you click it", async () => {
+  mockResults([]);
+  const editor = mockEditor();
+
+  const result = render(FormulaForm);
+  await fireEvent.click(result.getByText("2d6"));
+
+  expect(editor.focus).toBeCalledWith();
+  expect(resultsStore.append).toBeCalledWith(result2d6);
+  expect(editor.execCommand).toBeCalledWith("selectAll");
 });
