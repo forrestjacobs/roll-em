6ab9a83d44faaa72533b44272c64802bf99aee1d
Forrest Jacobs <forrestjacobs@gmail.com>
2020-10-03 00:25:51 -0400

    Test store entry

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,4 +1,4 @@
-import { DbV1, makeDb } from "./db";
+import { makeDb } from "./db";
 import { makeResultsStore, ResultsStore } from "./results-store";
 
 export { ResultSource, ResultsStoreState } from "./results-store";
@@ -6,14 +6,8 @@ export type { ResultsStore, StoredResult } from "./results-store";
 
 const BATCH_SIZE = 50;
 
-let db: Promise<DbV1> | undefined = undefined;
-function getDb(): Promise<DbV1> {
-  db ??= makeDb();
-  return db;
-}
-
 let resultsStore: ResultsStore | undefined = undefined;
 export function getResultsStore(): ResultsStore {
-  resultsStore ??= makeResultsStore(getDb(), BATCH_SIZE);
+  resultsStore ??= makeResultsStore(makeDb(), BATCH_SIZE);
   return resultsStore;
 }
---
+++ src/stores/test.ts
@@ -0,0 +1,22 @@
+import { getResultsStore } from ".";
+import { makeDb } from "./db";
+import { makeResultsStore } from "./results-store";
+
+jest.mock("./db", () => ({
+  makeDb: jest.fn(() => "db"),
+}));
+jest.mock("./results-store", () => ({
+  makeResultsStore: jest.fn(() => "results-store"),
+}));
+
+test("it can get a results store", () => {
+  expect(getResultsStore()).toBe("results-store");
+  expect(makeResultsStore).toBeCalledWith("db", expect.any(Number));
+});
+
+test("it uses the same db and results store", () => {
+  getResultsStore();
+  getResultsStore();
+  expect(makeDb).toBeCalledTimes(1);
+  expect(makeResultsStore).toBeCalledTimes(1);
+});
