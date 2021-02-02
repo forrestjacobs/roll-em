c257bb9b99277eb9cf7caedc87fb1cdc9395bcf9
Forrest Jacobs <forrestjacobs@gmail.com>
2021-02-02 00:18:07 -0500

    Test results from local storage before converting
    Inspired by https://startup-cto.net/10-bad-typescript-habits-to-break-this-year/

--- src/stores/migrate-from-v0.ts
+++ src/stores/migrate-from-v0.ts
@@ -1,11 +1,11 @@
-type ResultTermV0 =
-  | { type: "number"; value: number }
-  | { type: "roll"; count: number; sides: number; value: number[] };
-
-interface StoredResultV0 {
-  id: string;
-  result: ResultTermV0[];
-}
+type RollTermV0 = {
+  type: "roll";
+  count: number;
+  sides: number;
+  value: number[];
+};
+type NumberTermV0 = { type: "number"; value: number };
+type ResultTermV0 = RollTermV0 | NumberTermV0;
 
 const LOCAL_STORAGE_KEY_V0 = "results";
 
@@ -20,18 +20,37 @@ function makeResultsStore(db: IDBDatabase): IDBObjectStore {
   return store;
 }
 
+function isV0Term(obj: unknown): obj is ResultTermV0 {
+  return (
+    obj !== undefined &&
+    obj !== null &&
+    (((obj as ResultTermV0).type === "number" &&
+      typeof (obj as NumberTermV0).value === "number") ||
+      ((obj as ResultTermV0).type === "roll" &&
+        typeof (obj as RollTermV0).count === "number" &&
+        typeof (obj as RollTermV0).sides === "number" &&
+        Array.isArray((obj as RollTermV0).value) &&
+        (obj as RollTermV0).value.every((e) => typeof e === "number")))
+  );
+}
+
 function migrateLocalStorageResults(store: IDBObjectStore): void {
   const resultsItem = localStorage.getItem(LOCAL_STORAGE_KEY_V0);
   if (resultsItem === null) {
     return;
   }
 
-  const results = JSON.parse(resultsItem) as StoredResultV0[];
-  for (let i = results.length - 1; i >= 0; i--) {
-    store.add({
-      date: 0,
-      result: results[i].result,
-    });
+  const results = JSON.parse(resultsItem);
+  if (Array.isArray(results)) {
+    for (let i = results.length - 1; i >= 0; i--) {
+      const result = results[i]?.result;
+      if (Array.isArray(result) && result.every(isV0Term)) {
+        store.add({
+          date: 0,
+          result: result,
+        });
+      }
+    }
   }
 
   localStorage.removeItem(LOCAL_STORAGE_KEY_V0);
