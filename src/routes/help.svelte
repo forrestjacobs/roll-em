9c93d66b204c8d5c28241e7cc7c536a603c85852
Forrest Jacobs <forrestjacobs@gmail.com>
2021-11-21 23:55:15 -0500

    Add basic help page

---
+++ src/routes/help.svelte
@@ -0,0 +1,54 @@
+<script>
+  import Example from "../ui/Example.svelte";
+  import FormattedValue from "../ui/FormattedValue.svelte";
+</script>
+
+<style>
+  table {
+    border-collapse: collapse;
+    margin: 1em 0;
+  }
+
+  th {
+    text-align: left;
+    padding: 0.25em 1em;
+  }
+
+  th.result {
+    padding-left: 0;
+  }
+
+  .header {
+    padding: 1em 1em 0 1em;
+  }
+</style>
+
+<svelte:head>
+  <title>Help - Roll 'Em</title>
+</svelte:head>
+
+<div class="header">
+  <h2>What can I type to roll dice?</h2>
+</div>
+
+<table>
+  <thead>
+    <tr>
+      <th scope="col" class="type">Type</th>
+      <th scope="col" class="result">Result</th>
+    </tr>
+  </thead>
+  <Example value="d6 + 2">Rolls a six-sided die and adds two</Example>
+  <Example value="2d8">Rolls two eight-sided dice</Example>
+  <Example value="d6 + d8">Rolls a six and eight-sided die</Example>
+  <Example value="d4 + d6 + d8 + d10 + d12 + d20">
+    Rolls standard D&D dice
+  </Example>
+  <Example value="d100 + d%">
+    Both
+    {" "}<FormattedValue value="d100" />{" "}
+    and
+    {" "}<FormattedValue value="d%" />{" "}
+    roll 100-sided dice.
+  </Example>
+</table>
---
+++ src/ui/Example.svelte
@@ -0,0 +1,50 @@
+<script lang="ts" context="module">
+  import Result from "./Result.svelte";
+  import FormattedValue from "./FormattedValue.svelte";
+  import { parse, roll } from "../formula";
+
+  function maxRoller(sides: number): number {
+    return sides;
+  }
+</script>
+
+<script lang="ts">
+  export let value: string;
+</script>
+
+<style>
+
+  .type {
+    padding: 0.5em 0.75em;
+  }
+
+  .value {
+    background: var(--white);
+    padding: 0.25em;
+  }
+
+  .result {
+    padding: 0.5em 1em 0.5em 0;
+  }
+
+  .caption {
+    padding: 0 1em 1em 1em;
+    border-bottom: 1px solid var(--light);
+  }
+</style>
+
+<tr>
+  <td class="type">
+    <span class="value">
+      <FormattedValue value="{value}" />
+    </span>
+  </td>
+  <td class="result">
+    <Result result="{roll(parse(value), maxRoller)}" animated="{false}" />
+  </td>
+</tr>
+<tr>
+  <td colspan="2" class="caption">
+    <slot />
+  </td>
+</tr>
